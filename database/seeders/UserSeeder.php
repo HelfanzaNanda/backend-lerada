87fc51d2dd04aa852f2026e31787de76d86914e3
HelfanzaNanda <elvannnd@gmail.com>
2020-12-25 20:46:08 +0700

    gak jadi hapus, maap

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +16,29 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        User::create([
+            'name' => 'helfanza',
+            'email' => 'helfanza@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(12345678),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'nanda',
+            'email' => 'nanda@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(12345678),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'alfara',
+            'email' => 'alfara@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(12345678),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
